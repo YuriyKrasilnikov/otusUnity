e319408d75224904e8926ef1adfba5a791669d43
YuriyKrasilnikov <yury.krasilnikov@gmail.com>
2020-02-12 22:06:47 +0300

    In this homework we consolidate the skills of working with scenes and Animator Controller in Unity. 1. Take the project files from the materials for the lesson. 2. Add an animation of your character's death to Animator Controller. Make sure that after hitting the attacking character plays the death animation. Make sure that you can no longer interact with your character after the death. 3. Add another state for the fist attack (use animation from Z_attack_A).
    Rating criteria:
    1. An animation of death has been added to the Animation Controller.
    2. Death animation is played only once. The character remains in the last frame of the Death Animation.
    3. After the character's death, he no longer reacts to attempts to attack him again.
    4. The corresponding menu item in the component cannot activate an attack by a dead character.
    5. The zombie character uses the punched animation: Z_attack_A.

---
+++ 02_AnimationController/Assets/Scripts/Character.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public enum State
+    {
+        Idle,
+        RunningToEnemy,
+        RunningFromEnemy,
+        BeginAttack,
+        Attack,
+        BeginShoot,
+        Shoot,
+        BeginDeath,
+        Death
+    }
+
+    public enum Weapon
+    {
+        Pistol,
+        Bat,
+        Fist
+    }
+
+    public float runSpeed;
+    public float distanceFromEnemy;
+    public Transform target;
+    public Weapon weapon;
+    Animator animator;
+    Vector3 originalPosition;
+    Quaternion originalRotation;
+    State state = State.Idle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        originalPosition = transform.position;
+        originalRotation = transform.rotation;
+    }
+
+    [ContextMenu("Attack")]
+    void AttackEnemy()
+    {
+        if (state != State.Death && target.GetComponent<Character>().state != State.Death) {
+            switch (weapon)
+            {
+                case Weapon.Bat:
+                    state = State.RunningToEnemy;
+                    break;
+
+                case Weapon.Fist:
+                    state = State.RunningToEnemy;
+                    break;
+
+                case Weapon.Pistol:
+                    state = State.BeginShoot;
+                    break;
+            }
+        }
+    }
+
+    public void SetState(State newState)
+    {
+        state = newState;
+    }
+
+    public void TargetSetState(State newState)
+    {
+        target.GetComponent<Character>().SetState(newState);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        switch (state){
+            case State.Idle:
+                animator.SetFloat("speed", 0.0f);
+                transform.rotation = originalRotation;
+                break;
+
+            case State.RunningToEnemy:
+                animator.SetFloat("speed", runSpeed);
+                if (RunTowards(target.position, distanceFromEnemy))
+                {
+                    state = State.BeginAttack;
+                }
+                break;
+
+            case State.RunningFromEnemy:
+                animator.SetFloat("speed", runSpeed);
+                if (RunTowards(originalPosition, 0.0f))
+                {
+                    state = State.Idle;
+                }
+                break;
+
+            case State.BeginAttack:
+                animator.SetFloat("speed", 0.0f);
+                switch (weapon)
+                {
+                    case Weapon.Bat:
+                        animator.SetTrigger("attack");
+                        state = State.Attack;
+                        break;
+
+                    case Weapon.Fist:
+                        animator.SetTrigger("punch");
+                        state = State.Attack;
+                        break;
+                }
+                
+                
+                break;
+
+            case State.Attack:
+                break;
+
+            case State.BeginShoot:
+                animator.SetTrigger("shoot");
+                state = State.Shoot;
+                break;
+
+            case State.Shoot:
+                break;
+
+            case State.BeginDeath:
+                Debug.Log(gameObject.name + "dead!");
+                animator.SetTrigger("killed");
+                state = State.Death;
+                break;
+        }
+
+    }
+
+    bool RunTowards(Vector3 targetPosition, float distanceFromTarget)
+    {
+        Vector3 distance = targetPosition - transform.position;
+        Vector3 direction = distance.normalized;
+
+        transform.rotation = Quaternion.LookRotation(direction);
+
+        //Debug.Log($"{distance.magnitude}");
+
+        targetPosition -= direction * distanceFromTarget;
+        distance = targetPosition - transform.position;
+
+        Vector3 vector = direction * runSpeed;
+        if (vector.magnitude < distance.magnitude)
+        {
+            transform.position += vector;
+            return false;
+        }
+
+        transform.position = targetPosition;
+        return true;
+
+    }
+}
